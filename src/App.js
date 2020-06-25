64803071b3e049a3a5f708e37d084d8d65fc5da7
syawal2kalii <syawal20162016@gmail.com>
2020-06-25 16:58:21 +0800

    6_komsumsi_api

--- src/App.js
+++ src/App.js
@@ -3,50 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import List from './List'
 
-// Function Base
-// function Clicker() {
-//   function handleClick(e) {
-//     alert("berhasil mengklik");
-//     e.preventDefault()
-//   }
-
-//   return(
-//     <a href="#" onClick={handleClick}>Klik Bro Sis!</a>
-//   )
-// }
-
 class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      todoItem : '',
       items : []
     }
   }
 
-  handleSubmit = (event)=>{
-    event.preventDefault()
-    this.setState({
-      items : [...this.state.items,this.state.todoItem],
-      todoItem : ''
-    });
-  }
-
-  handleChange = (event)=>{
-    this.setState({
-      todoItem : event.target.value
-    })
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data => this.setState({items : data}))
   }
 
   render(){
+    const {items} = this.state
     return (
       <div>
-        <p>to do List</p>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.todoItem} onChange={this.handleChange}/>
-          <button>Add</button>
-        </form>
-        <List items={this.state.items} />
+        <ul>
+          {items.map((item,index) =>
+            <li key={index}> {item.name}</li>
+          )}
+        </ul>
       </div>
     )
   }
