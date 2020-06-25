8b54b5b94112136d9404762f97550f3a5fd87386
syawal2kalii <syawal20162016@gmail.com>
2020-06-25 11:23:40 +0800

    3_state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Biodata(props) {
-  return <span> Umur {props.age1}</span>
-}
+class Timer extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      time : props.start
+    }
+  }
 
-function Greeting(props) {
-  return <h1>Halo {props.name} <Biodata age1={props.age}/></h1>
-}
+  // Lifecycle
+  // component didmount = setelah komponen selesai di render
+  componentDidMount(){
+    this.addInterval = setInterval(()=> this.increase(),1000)
+  }
 
+  componentWillUnmount(){
+    clearInterval(this.addInterval);
+  }
+
+  increase() {
+    this.setState(
+      (state,props) => ({time: parseInt(state.time) + 1})
+    )  
+  }
+  render(){
+    return (
+    <div>{this.state.time} Detik</div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Greeting name="Syawal" age="10"/>
-        {/* reusable */}
-        <Greeting name="Syawal" age="22"/>
+        <Timer start="0"/>
+        <Timer start="5"/>
       </header>
     </div>
   );
