066c2eac34551bb02d39e1bcb6492640ed90261e
syawal2kalii <syawal20162016@gmail.com>
2020-06-25 20:21:53 +0800

    7_single_page_aplications

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import List from './List'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+
+function Home() {
+  return <h2>Halaman Home</h2>
+}
+
+function ListView() {
+  return (
+    <div>
+      <h2> Semua User</h2>
+      <ul>
+        <Link to='users/muh'>Muh</Link> <br/>
+        <Link to='users/syawal'>Syawal</Link>
+      </ul>
+    </div>
+  )
+}
+
+function DetailView({match}) {
+  return <h2> Ini Halaman {match.params.name}</h2>
+}
+
+function NoMatch() {
+  return <h2>404, Halaman tidak ditemukan</h2>
+}
+
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      items : []
-    }
-  }
-
-  componentDidMount(){
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(data => this.setState({items : data}))
-  }
-
   render(){
-    const {items} = this.state
     return (
-      <div>
-        <ul>
-          {items.map((item,index) =>
-            <li key={index}> {item.name}</li>
-          )}
-        </ul>
-      </div>
+      <BrowserRouter>
+        <div>
+        <nav>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/users'>User</Link></li>
+        </nav>
+        <main>
+          <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/users' exact component={ListView}/>
+            <Route path='/users/:name' exact component={DetailView}/>
+            <Route component={NoMatch} />
+          </Switch>
+        </main>      
+        </div>
+      </BrowserRouter>
     )
   }
 }
